21c89e8a2addfa6cad953642bd491039a2bdbd19
Anik7303 <anik7303@gmail.com>
2020-05-21 20:18:11 +0600

    finishing up section 09

---
+++ shared.js
@@ -0,0 +1,36 @@
+var backdrop = document.querySelector(".backdrop");
+var modal = document.querySelector(".modal");
+var choosePlanButtonList = document.querySelectorAll(".plan button");
+var toggleButton = document.querySelector(".toggle-button");
+var mobileNav = document.querySelector(".mobile-nav");
+var modalCancelButton = document.querySelector(".modal__action--negative");
+
+for (var i = 0; i < choosePlanButtonList.length; i++) {
+    choosePlanButtonList[i].addEventListener("click", function () {
+        backdrop.classList.add("open");
+        modal.classList.add("open");
+    });
+}
+
+var closeModal = function () {
+    if (modal) {
+        modal.classList.remove("open");
+    }
+    backdrop.classList.remove("open");
+};
+
+backdrop.addEventListener("click", function () {
+    closeModal();
+    mobileNav.classList.remove("open");
+});
+
+toggleButton.addEventListener("click", function () {
+    mobileNav.classList.add("open");
+    backdrop.classList.add("open");
+});
+
+if (modalCancelButton) {
+    modalCancelButton.addEventListener("click", function () {
+        closeModal();
+    });
+}
